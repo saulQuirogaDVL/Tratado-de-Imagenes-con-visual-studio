b98f0bdcaebf0e5b5fd1aa523aabaa344ee1a3fe
saulQuirogaDVL <saulquirogacasDVL@gmail.com>
2021-01-15 18:44:09 -0400

    Agregar archivos de proyecto.

---
+++ Proyecto_Procesamiento_Digital/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Proyecto_Procesamiento_Digital
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfdGuardar = new System.Windows.Forms.SaveFileDialog();
+            this.opfAbrir = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pixeleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pixelearZonaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pctLienzo = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLienzo)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // opfAbrir
+            // 
+            this.opfAbrir.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.pixeleadoToolStripMenuItem,
+            this.pixelearZonaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1184, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(145, 26);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(145, 26);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(145, 26);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // pixeleadoToolStripMenuItem
+            // 
+            this.pixeleadoToolStripMenuItem.Name = "pixeleadoToolStripMenuItem";
+            this.pixeleadoToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.pixeleadoToolStripMenuItem.Text = "Pixeleado";
+            this.pixeleadoToolStripMenuItem.Click += new System.EventHandler(this.pixeleadoToolStripMenuItem_Click);
+            // 
+            // pixelearZonaToolStripMenuItem
+            // 
+            this.pixelearZonaToolStripMenuItem.Name = "pixelearZonaToolStripMenuItem";
+            this.pixelearZonaToolStripMenuItem.Size = new System.Drawing.Size(113, 24);
+            this.pixelearZonaToolStripMenuItem.Text = "Pixelear Zona";
+            this.pixelearZonaToolStripMenuItem.Click += new System.EventHandler(this.pixelearZonaToolStripMenuItem_Click);
+            // 
+            // pctLienzo
+            // 
+            this.pctLienzo.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pctLienzo.Location = new System.Drawing.Point(25, 32);
+            this.pctLienzo.Margin = new System.Windows.Forms.Padding(4);
+            this.pctLienzo.Name = "pctLienzo";
+            this.pctLienzo.Size = new System.Drawing.Size(700, 600);
+            this.pctLienzo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctLienzo.TabIndex = 2;
+            this.pctLienzo.TabStop = false;
+            this.pctLienzo.Click += new System.EventHandler(this.pctLienzo_Click);
+            this.pctLienzo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pctLienzo_MouseMove);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(748, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(424, 263);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nivel de Pixeleado";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(401, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Revertir Todo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(165, 211);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(129, 43);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "LUPA";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(401, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "5";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 38);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(397, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.DragOver += new System.Windows.Forms.DragEventHandler(this.trackBar1_DragOver);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox2.Location = new System.Drawing.Point(1028, 302);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Padding = new System.Windows.Forms.Padding(5);
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox1.Location = new System.Drawing.Point(1028, 415);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(748, 317);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(241, 198);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cambiar Color";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 127);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(229, 44);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "VERDE CLARO";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 77);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(229, 44);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "BLANCO";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(229, 44);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "NEGRO";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 657);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pctLienzo);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLienzo)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog sfdGuardar;
+        private System.Windows.Forms.OpenFileDialog opfAbrir;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pctLienzo;
+        private System.Windows.Forms.ToolStripMenuItem pixeleadoToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem pixelearZonaToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Proyecto_Procesamiento_Digital/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Procesamiento_Digital
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap ImagenOriginal;
+        Bitmap ImagenResultado;
+        int AnchoImagen, AltoImagen,cont=0;
+        Pen color;
+
+        private void pixeleadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int cont = 16, contPro = cont * cont, cont1 = cont, cont2 = cont;
+            int rojo = 0, verde = 0, azul = 0;
+            Color miColor = new Color();
+            Color nuevoColor = new Color();
+
+            for (int x = 0; x < AnchoImagen; x += cont)
+            {
+                cont2 = cont;
+                for (int y = 0; y < AltoImagen; y += cont)
+                {
+                    for (int i = x; i < cont1; i++)
+                    {
+                        for (int j = y; j < cont2; j++)
+                        {
+                            miColor = ImagenOriginal.GetPixel(i, j);
+                            rojo += miColor.R;
+                            verde += miColor.G;
+                            azul += miColor.B;
+
+                        }
+                    }
+                    rojo = Convert.ToInt32(rojo / contPro);
+                    verde = Convert.ToInt32(verde / contPro);
+                    azul = Convert.ToInt32(azul / contPro);
+                    for (int i = x; i < cont1; i++)
+                    {
+                        for (int j = y; j < cont2; j++)
+                        {
+                            nuevoColor = Color.FromArgb(255, rojo, verde, azul);
+                            ImagenResultado.SetPixel(i, j, nuevoColor);
+                        }
+                    }
+                    if ((cont2 + cont) < AltoImagen)
+                    {
+                        cont2 += cont;
+                    }
+                    else
+                    {
+                        cont2 = AltoImagen;
+                    }
+                }
+                if ((cont1 + cont) < AnchoImagen)
+                {
+                    cont1 += cont;
+                }
+                else
+                {
+                    cont1 = AnchoImagen;
+                }
+            }
+            pctLienzo.Image = ImagenResultado;
+            pctLienzo.Refresh();
+        }
+
+
+        int px0, px1, px2;
+        int py0, py1, py2;
+
+        private void trackBar1_DragOver(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        int trackVal = 5;
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            button1.Text = "Nivel: " + trackBar1.Value;
+            trackVal = int.Parse(trackBar1.Value.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int xrdt1 = (ImagenOriginal.Width * px1) / pctLienzo.Width;
+                int yrdt1 = (ImagenOriginal.Height * py1) / pctLienzo.Height;
+                int xrdt2 = (ImagenOriginal.Width * px2) / pctLienzo.Width;
+                int yrdt2 = (ImagenOriginal.Height * py2) / pctLienzo.Height;
+                int cont = 0;
+                Color miColor = new Color();
+                for (int i = xrdt1; i < xrdt2; i++)
+                {
+                    for (int j = yrdt1; j < yrdt2; j++)
+                    {
+                        if (cont == trackVal || j == yrdt1)
+                        {
+                            miColor = ImagenOriginal.GetPixel(i, j);
+                            cont = 0;
+                        }
+                        ImagenResultado.SetPixel(i, j, miColor);
+                        cont++;
+                    }
+                }
+                pctLienzo.Image = ImagenResultado;
+                pctLienzo.Refresh();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Imagen No Instanciada");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ImagenOriginal = (Bitmap)(Bitmap.FromFile(opfAbrir.FileName));
+                AnchoImagen = ImagenOriginal.Width;
+                AltoImagen = ImagenOriginal.Height;
+                pctLienzo.Image = ImagenOriginal;
+                ImagenResultado = ImagenOriginal;
+                pctLienzo.Refresh();
+                checkBox1.Checked = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Imagen No Instanciada");
+            }
+        }
+
+        private void pixelearZonaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = true;
+        }
+
+
+
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                MessageBox.Show("encendido");
+                cont = 1;
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Apagado");
+                cont = 0;
+                pictureBox2.Visible = false;
+                pictureBox2.Image = pictureBox1.Image;
+                pictureBox2.Location=new Point (1028, 302);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            color = new Pen(Brushes.Black,5);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            color = new Pen(Brushes.White,5);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            color = new Pen(Brushes.LightGreen,5);
+        }
+
+        private void marcaNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int xrdt1 = (ImagenOriginal.Width * px1) / pctLienzo.Width;
+            int yrdt1 = (ImagenOriginal.Height * py1) / pctLienzo.Height;
+            int xrdt2 = (ImagenOriginal.Width * px2) / pctLienzo.Width;
+            int yrdt2 = (ImagenOriginal.Height * py2) / pctLienzo.Height;
+            int cont = 0;
+            Color miColor = new Color();
+            for (int i = xrdt1; i < xrdt2; i++)
+            {
+                for (int j = yrdt1; j < yrdt2; j++)
+                {
+                    if (cont == 50 || j == yrdt1)
+                    {                 
+                       miColor=ImagenOriginal.GetPixel(i, j);
+                        cont = 0;
+                    }
+                    ImagenResultado.SetPixel(i, j, miColor);
+                    cont++;
+                }
+            }
+            pctLienzo.Image = ImagenResultado;
+            pctLienzo.Refresh();
+        }
+
+        int pnum = 0;
+        Color micol;
+        private void pctLienzo_MouseMove(object sender, MouseEventArgs e)
+        {
+            px0 = e.X;
+            py0 = e.Y;
+        }
+
+        private void pctLienzo_Click(object sender, EventArgs e)
+        {
+
+            try { 
+            if (cont == 0)
+            {
+                if (pnum == 0)
+                {
+                    px1 = px0;
+                    py1 = py0;
+                    pnum = 1;
+                    Color micolor = new Color();
+
+                }
+                else
+                {
+                    pctLienzo.Image = ImagenOriginal;
+                    pctLienzo.Refresh();
+                    px2 = px0;
+                    py2 = py0;
+                    pnum = 0;
+                    Graphics g;
+                    g = pctLienzo.CreateGraphics();
+                    g.DrawRectangle(color, px1, py1, px2 - px1, py2 - py1);
+                }
+            }
+            else
+            {
+                int xrdt = (ImagenOriginal.Width * px0) / pctLienzo.Width;
+                int yrdt = (ImagenOriginal.Height * py0) / pctLienzo.Height;
+                Bitmap imagenR = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+                pictureBox2.Left = px0 + 10;
+                pictureBox2.Top = py0 + 35;
+                Color micolor = new Color();
+                int contX = 4, contY = 4;
+
+                for (int i = 0; i < pictureBox2.Width; i++)
+                {
+                    for (int j = 0; j < pictureBox2.Height; j++)
+                    {
+                        //if (contY + j % 4 == 4||j==0)
+                        //{
+                        micol = ImagenOriginal.GetPixel(xrdt + i, yrdt + j);
+                        //}
+
+                        imagenR.SetPixel(i, j, micol);
+
+                    }
+                }
+                pictureBox2.Image = imagenR;
+                pictureBox2.Refresh();
+            }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (opfAbrir.ShowDialog() == DialogResult.OK)
+            {
+                ImagenOriginal = (Bitmap)(Bitmap.FromFile(opfAbrir.FileName));
+                AnchoImagen = ImagenOriginal.Width;
+                AltoImagen = ImagenOriginal.Height;
+                pctLienzo.Image = ImagenOriginal;
+                ImagenResultado = ImagenOriginal;
+                checkBox1.Checked = false;
+            }
+        }
+    }
+}
